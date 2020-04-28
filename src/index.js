39347b315aa76e85367816b8a09a68a375acbb7b
Lwithwma Brahma <lwith.brah@gmail.com>
2020-04-28 12:56:38 +0530

    adding CardList.js file and modifying  index.js file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App'; //App.js
-import Card from './Card'; //card.js
+//import Card from './Card'; //card.js
 import CardList from './CardList';
 import 'tachyons';
 import {robots} from './robots'; //use ./fileName if the file is in same directory
@@ -16,13 +16,7 @@ import * as serviceWorker from './serviceWorker';
 );*/
 //div is use to return single element
 ReactDOM.render(
-	<div>
-	  <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-	  <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-	  <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-	  <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
-	  <Card id={robots[4].id} name={robots[4].name} email={robots[4].email}/>
-	</div> 
+	<CardList robots={robots}/>
 	,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
